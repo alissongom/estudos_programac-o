f05fa33447ad534316bbba276a1fa60598b792cd
Alisson <125320258+alissongom@users.noreply.github.com>
2023-04-12 18:56:30 -0300

    Create tratamento_de_exceções.py

---
+++ tratamento_de_exceções.py
@@ -0,0 +1,15 @@
+# Tratamento de exceções
+try:
+    # Código que pode levantar exceções
+    x = int(input("Digite um número: "))
+    y = 10 / x
+    print(y)
+except ZeroDivisionError:
+    # Tratamento de exceção quando há uma divisão por zero
+    print("Não é possível dividir por zero")
+except ValueError:
+    # Tratamento de exceção quando o usuário não digita um número válido
+    print("Por favor, digite um número válido")
+except:
+    # Tratamento de exceção genérico para qualquer outro tipo de exceção
+    print("Ocorreu um erro")

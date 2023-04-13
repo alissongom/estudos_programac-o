fb72741a1714cd66be9a0bf720d91f96f058de4e
Alisson <125320258+alissongom@users.noreply.github.com>
2023-04-13 17:47:15 -0300

    Update python.py

--- python.py
+++ python.py
@@ -39,177 +39,99 @@ print("Me llamo:", name, surname, ". Mi edad es:", age, ". Y mi alias es:", alia
 
 
 # operadores dos tipos primitivos de dados
-
 # operadores aritméticos
-
 t = 28
-
 oi = 21
-
 print(t ** oi) # exponencial
-
 print(t * oi) # multiplicação
-
 print(t / oi) # divisão
-
 print(t // oi) # divisao_int
-
 print(t % oi) # resto_divisao
-
 print(t + oi) # adição
-
 print(t - oi) # subtração
-
-# calculo grande precisa da logica de pemdas
-
+# calculo grande precisa usa logica de pemdas
 r = (28 + 72) * 838 ** 92 // 82 % 82 / 1 - 98
-
 print(r)
 
 # operador de atribuição composta
-
 a = 5
-
 b = 10
-
 c = 15
-
 a += 2  # equivalente a a = a + 2 ( com adição é possível junta str )
-
 print(a)  # 7
-
 b -= 4  # equivalente a b = b - 4
-
 print(b)  # 6
-
 c *= 2  # equivalente a c = c * 2
-
 print(c)  # 30
-
 a /= 2 # equivalente a a = a / 2
-
 print(a)
-
 b %= 2 # equivalente a b = b % 2
-
 print(b)
-
-c //= 3 # equivalente a c = c // 3
-
+c //= 3 # equivalente a c = c // 3# bool
 print(c)
-
 b **= 3 # equivalente a b = b ** 3
-
 print(b)
 
-# operadores primitivos
-
+# operadores de comparação
 y = 48
-
 yu = 10
-
 uy = 29
-
-print(y > uy) #True
-
-print(y >= yu) #True
-
-print(uy < yu) #False
-
-print(uy <= yu) #False
-
-print(uy == y) #False
-
-print(y != uy) #True
+print(y > uy) # True
+print(y >= yu) # True
+print(uy < yu) # False
+print(uy <= yu) # False
+print(uy == y) # False
+print(y != uy) # True
 
 # pode usa operadores aritméticos com operadores de comparação juntos
-
 ret = 19
-
 pill = 38
-
 ter = 21
-
 print((38 * ter) > pill)
-
 print(ret >= (ter // pill))
-
 print(ter < (29 + 21))
-
 print(ter <= (ret % pill))
-
 print(pill == (ter + 18))
-
 print(ter != (10 + ret))
 
 # para fazer comparação com str com outro tipo de dado só é possível com o operador de igualdade == e diferente !=
-
 xz = 3
-
 cz = 'palavra'
-
 print(xz == cz)
-
 print(xz != cz)
 
-# só é possível fazer comparação com o str se for com o mesmo tipo de dado a comparação lexicografica
-
+# para usa operadores de comparação com str precisa ser do mesmo tipo de dado e a comparação é feita pela comparação lexicografica ( tabela unicode )
 er = 'gato'
-
 qs = 'largato'
-
 li = 'cachorro'
-
-print(li > qs) #False
-
-print(qs < er) #False
-
-print(li > er) #False
-
-print(qs > li) #True
+print(li > qs) # False
+print(qs < er) # False
+print(li > er) # False
+print(qs > li) # True
 
 # operadores logicos and, or, not
-
+# and
 cu = True
-
 cool = 18
-
-print(cool >= 12 and cu)#and só retorna True se os dois valores for verdadeiro
+print(cool >= 12 and cu)# and só retorna True se os dois valores for verdadeiro
 
 # or
-
 ver = 12
-
 fg = True
-
 print(ver > 18 or fg) # or retorna True se 1 dos valores for verdadeiro
 
 # e tem o operador not que inverte o valor de True para False e False para True
-
 kj = 18
-
 pr = False
-
 print(not(kj > 18 or pr))
 
-#
-
-#
-
-#
-
-# Operações Com String | Operador de Acesso das Strings [indice] (o espaço também conta como caractere)
-
+# operações com str | manipulação de str
+# operador de acesso é entre [colchetes] e o índice positivo sempre começa pelo zero e o índice negativo começa pelo -1 ( o espaço também conta como caractere )
 # indices positivo
-
 qpw = 'consolação'
-
 print('primeira letra da str: ',qpw[0]) # c
-
 print('ultima letra da str: ',qpw[9]) # o
-
 print('pega a letra L da str: ',qpw[5]) # l
-
 print('pega algumas letras da palavra: ',qpw[0:1:3:4])
 
 print('inverte a str: ',qpw[::-1]

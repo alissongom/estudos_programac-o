eee11f7f98499889241f0d58c2e21fce26eea21b
Alisson <125320258+alissongom@users.noreply.github.com>
2023-04-13 20:28:26 -0300

    Update python.py

--- python.py
+++ python.py
@@ -125,8 +125,7 @@ kj = 18
 pr = False
 print(not(kj > 18 or pr))
 
-# operações com str | manipulação de str
-# operador de acesso é entre [colchetes] e o índice positivo sempre começa pelo zero e o índice negativo começa pelo -1 ( o espaço também conta como caractere )
+# operações com str | operador de acesso é entre [colchetes] e o índice positivo sempre começa pelo zero e o índice negativo começa pelo -1 ( o espaço também conta como caractere )
 # indices positivo
 qpw = 'consolação'
 print('primeira letra da str: ',qpw[0]) # c
@@ -134,82 +133,48 @@ print('ultima letra da str: ',qpw[9]) # o
 print('pega a letra L da str: ',qpw[5]) # l
 print('pega algumas letras da palavra: ',qpw[0:1:3:4])
 
-print('inverte a str: ',qpw[::-1]
-
 # indices negativos
-
 print('pega a primeira letra da str: ',qpw[-10]) # c
-
 print('pega a ultima letra da str: ', qpw[-1]) # 0
-
 print('pega a letra L da str: ',qpw[-5]) # l
+print('inverte a str: ',qpw[::-1])
 
 # também é possível fazer uma sub-str que é uma str feita pela str original
-
 print(qpw[3:6]) #palavra sol
-
 print(qpw[-4:]) #palavra ação
-
 print(qpw[3:7]) #palavra sola
-
 print(qpw[:7]) #palavra consola
 
-# operadores de string ( * repete a str N vezes ) ( + Concatena (une) duas strings em uma única )
-
+# operadores de string ( * repete a str N vezes ) ( + Concatena (une) duas strings em uma única str )
 jk = 'belo'
-
 jn = 'horizonte'
-
 # repetição
-
 print(4 * jk) # retorna belobelobelobelo
-
 print((jk + ',') * 4) # retorna belo 4 vezes com vírgula
-
 # cocatenacão
-
 print(jk + jn)
-
-print(jk + ' ' + jn)
-
+print(jk + ' ' + jn) # a vírgula com um espaço dentro serve para dar espaço entre as str
 print(jk + ' monte')
 
 # também tem os operadores para verificar se uma str esta esta contida na outra str
-
 # `in` retorna True se a str1 estiver contida na str2
-
 # `not in` retorna True se a str1 NÃO estiver contida na str2
-
 s1 = 'consolação'
-
 s2 = 'sola'
-
 print(s1 in s2) # False
-
 print(s2 in s1) # True
-
 print('sola' in s1) # True
-
 print('sola' not in s2) # False
 
 # Metodos das Str ( Manipulação de Str )
-
 palavra = 'consolação'
-
 print(len(palavra)) # função `len()` retorna quantas caracteres a str tem
-
 s1 = 'Belo Horizonte'
-
 s2 = 'Esta é uma frase, com uma vírgula.'
-
 s3 = ' palavra com espaços '
-
 print(s1.upper()) # todas as letras maiúsculas ( .isupper retorna True se tiver maiúsculo )
-
 print(s1.lower()) # todas as letras minúsculas ( .islower retornar True se tiver minúsculo )
-
 print(s2.title()) # primeira letra de cada palavra em maísculo ( .istitle retorna True se primeira letra de cada palavra tiver maiúscula )
-
 print(s1.replace('Horizonte', 'Monte')) # substitui 'Horizonte' por 'Monte'
 
 print(s1.startswith('Belo')) # verifica se a string começa com 'Belo'

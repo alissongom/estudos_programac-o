a1525f207bf845dc1809e51b1eaa20a4a07009f1
Alisson <125320258+alissongom@users.noreply.github.com>
2023-04-14 15:45:26 -0300

    Update python.py

--- python.py
+++ python.py
@@ -165,133 +165,88 @@ print(s2 in s1) # True
 print('sola' in s1) # True
 print('sola' not in s2) # False
 
-# Metodos das Str ( Manipulação de Str )
+# Metodos para Str ( Manipulação de Str )
 palavra = 'consolação'
 print(len(palavra)) # função `len()` retorna quantas caracteres a str tem
-s1 = 'Belo Horizonte'
+s1 = 'belo Horizonte'
 s2 = 'Esta é uma frase, com uma vírgula.'
 s3 = ' palavra com espaços '
+s4 = '231'
 print(s1.upper()) # todas as letras maiúsculas ( .isupper retorna True se tiver maiúsculo )
 print(s1.lower()) # todas as letras minúsculas ( .islower retornar True se tiver minúsculo )
 print(s2.title()) # primeira letra de cada palavra em maísculo ( .istitle retorna True se primeira letra de cada palavra tiver maiúscula )
 print(s1.replace('Horizonte', 'Monte')) # substitui 'Horizonte' por 'Monte'
-
-print(s1.startswith('Belo')) # verifica se a string começa com 'Belo'
-
+print(s1.startswith('belo')) # verifica se a string começa com 'Belo'
 print(s1.endswith('Monte')) # verifica se a string termina com 'Monte'
-
 print(s2.find('frase')) # retorna o índice da primeira ocorrência da palavra frase ( retorna -1 se não for encontrado )
-
 print(s2.split()) # particiona a string, que são separadas por um espaço
-
 print(s2.split(',')) # particiona a string, que são separadas por ','
-
 print(s3.strip()) # remove os espaços extras no início e fim da string
+print(s1.capitalize()) # retorna a primeira letra da palavra maiúsculo e o resto minúsculo
+print(s2.count('a')) # retorna quantas vezes um elemento aparece na str
+print(s4.isnumeric()) # retorna True se a str tiver só números, retorna False se tiver letras ou numeros com ponto ou decimal
 
 # Os métodos das str podem, também, ser utilizados diretamente na declaração do valor.
-
 print('nome'.upper())
-
 a1 = 'console'
-
 ae = a1.upper().replace('CO', 'C') # também é possível utilizar esse método
-
 print(ae)
 
-#
-
-#
-
-#
 
 # Conversão dos tipos de Dados ( converte um tipo de dado em outro, por meio de uma função de conversão )
-
 nascimento = 1986
-
 ano_atual = 2022
-
 idade = ano_atual - nascimento
-
 print('Eu tenho ' + str(idade) + ' anos')
 
 # Conversão de inteiro para float
-
 i = 10
-
 f = float(i)
-
 print(f) # saída: 10.0
 
 # Conversão de inteiro para bool
-
 i = 1
-
 b = bool(i)
-
 print(b) # saída: True
 
 # Conversão de inteiro para string
-
 i = 20
-
 s = str(i)
-
 print(s) # saída: '20'
 
 # Conversão de float para inteiro
-
 f = 3.14159
-
 i = int(f)
-
 print(i) # saída: 3
 
 # Conversão de float para bool
-
 f = 0.0
-
 b = bool(f)
-
 print(b) # saída: False
 
 # Conversão de float para string
-
 f = 2.71828
-
 s = str(f)
-
 print(s) # saída: '2.71828'
 
 # Conversão de bool para inteiro
-
 b = True
-
 i = int(b)
-
 print(i) # saída: 1
 
 # Conversão de bool para float
-
 b = False
-
 f = float(b)
-
 print(f) # saída: 0.0
 
 # Conversão de bool para string
-
 b = True
-
 s = str(b)
-
 print(s) # saída: 'True'
 
 # Conversão de string para inteiro
-
 s = '123'
-
 i = int(s)
-
 print(i) # saída: 123
 
 # Conversão de string para float
